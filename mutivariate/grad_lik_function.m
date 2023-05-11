f142b08f1611a05a8d3633c7a55c2d9f9ee00e0d
Justin Kang <justin_kang@berkeley.edu>
2023-05-10 18:17:59 -0700

    multidimensional gradient and liklihood functions implemented

--- mutivariate/grad_lik_function.m
+++ mutivariate/grad_lik_function.m
@@ -1,37 +1,46 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This function computes an approximaiton to the gradient of a trucated 
-% non-negative normal distribution with 
-% 
+% This function computes an approximation to the gradient of a trucated 
+% non-negative normal distribution in the natural parameters U (varaince)
+% and V = U*mu, where mu is the mean
+%
+% grad_lik_function(Y,U,V)
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [gradU, gradv] = grad_lik_function(Y,U,V)
     [~,n] = size(Y);
     S = Y <= 0;
     Sc =~S;
-    invU = inv(U);
-    gradU = n*0.5*invU;
-    gradU = gradU + n*0.25*(invU*V*V'*invU);
+    L = chol(U);
+    invL = inv(L);
+    invU = invL*invL';
+    gradU = n*(0.5*invU + 0.25*(invU*(V*V')*invU));
     gradv = -n*0.5*invU*V;
-    n_mc = 100000;
+    n_mc = 500000;
     for i=1:n
-        % For readability
         si = S(:,i);
+        d_si = nnz(si);
         sci = Sc(:,i);
         y_sc = Y(sci, i);
-        Us = U(si,si);
-        invM = inv(chol(Us));
-        invUs = invM*invM';
-        mu = invUs*(U(si, sci)*y_sc + 0.5*V(si));
-        x = mvnrnd(mu,invUs/2, n_mc)';
-        % Check which samples to reject
-        x = x(:,sum(x < 0,1) == nnz(si));
-        [~,n_acc] = size(x);
-        % Gradient Updates
         gradU(sci,sci) = gradU(sci,sci) + -y_sc*y_sc';
-        gradU(si,si) = gradU(si,si) + -x*x';
-        grad_sc_s = -repmat(y_sc,1,n_acc)*x';
-        gradU(sci,si) = gradU(sci,si) + grad_sc_s;
-        gradU(si,sci) = gradU(si,sci) + grad_sc_s';
         gradv(sci) = gradv(sci) - y_sc;
-        gradv(si) = -mean(x,2);
+        if d_si == 0
+            continue
+        else
+            Us = U(si,si);
+            invM = inv(chol(Us));
+            invUs = invM*invM';
+            mu = -invUs*(U(si, sci)*y_sc + 0.5*V(si));
+            x = mvnrnd(mu,invUs/2, n_mc)';
+            % Check which samples to reject
+            x = x(:,sum(x < 0,1) == nnz(si));
+            [~,n_acc] = size(x);
+            % Gradient Updates
+            mu_x = mean(x,2);
+            gradU(si,si) = gradU(si,si) + -x*x'/n_acc;
+            grad_sc_s = -y_sc*mu_x';
+            gradU(sci,si) = gradU(sci,si) + grad_sc_s;
+            gradU(si,sci) = gradU(si,sci) + grad_sc_s';
+            gradv(si) = gradv(si) - mu_x;
+        end
     end
 end
--- mutivariate/multivar_MLE_calc.m
+++ mutivariate/multivar_MLE_calc.m
@@ -1,11 +1,32 @@
 clear;clc;
 rng(0)
 d = 10;
-n = 2;
+n = 100;
 A = eye(d) + randn(d,d)./(2*d);
 sigma = A*A';
 mu = zeros(d,1);
 Y = max(0, mvnrnd(mu,sigma,n)');
 U = inv(sigma)/2;
 V = -2*U*mu;
-grad = grad_lik_function(Y,U,V);
+for i=1:5
+    [gradu, gradv] = grad_lik_function(Y,U,V)
+end
+for i=1:5
+    lik = lik_func_multivar(Y,U,V)
+end
+%% Compute the gradient via finite differences
+eps = 1e-3;
+dV = zeros(d,1);
+dU = zeros(d,d);
+for i=1:d
+    dV(i) = eps;
+    emp_gradv(i) = (lik_func_multivar(Y,U,V+dV) - lik_func_multivar(Y,U,V-dV))/(2*eps);
+    dV(i) = 0;
+end
+for i=1:d
+    for j=1:d
+        dU(i,j) = eps;
+        emp_gradU(i,j) = (lik_func_multivar(Y,U+dU,V) - lik_func_multivar(Y,U-dU,V))/(2*eps);
+        dU(i,j) = 0;
+    end
+end
